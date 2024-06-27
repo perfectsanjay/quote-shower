cb86b09b8941b0b4a747d214055bbeee04639d09
Sanjay Singh <sanjaysingh@Sanjays-MBP.bbrouter>
2024-06-27 18:39:34 +0530

    quote project complete

--- script.js
+++ script.js
@@ -3,6 +3,20 @@ const quoteText = document.getElementById('quote')
 const authorText = document.getElementById('author')
 const twitterBtn = document.getElementById('twitter')
 const quoteBtn = document.getElementById('new-quote')
+const load = document.getElementById('loader')
+
+function loading(){
+    load.hidden = false
+    quoteContainer.hidden = true
+}
+
+function complete(){
+    if(!load.hidden){
+        quoteContainer.hidden = false
+        load.hidden = true
+    }
+}
+
 
 let data = []
 function getText(){
@@ -25,18 +39,20 @@ function getText(){
 }
 
 async function getData(){
+    loading()
     const url  = 'https://type.fit/api/quotes'
     try{
         const response = await fetch(url)
         data = await response.json()
         getText()
+        complete()
     
     }catch(error){
         console.log(error)
     }
     
 }
-getData()
+
 function TwitterLink(){
    const url = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
    window.open(url,'_blank')
@@ -44,3 +60,5 @@ function TwitterLink(){
 
 quoteBtn.addEventListener('click',getText)
 twitterBtn.addEventListener('click',TwitterLink)
+
+getData()
